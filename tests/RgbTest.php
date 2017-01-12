ec4f9429df68438ec7b37bca24253c8ea88c2577
Freek Van der Herten <freek@spatie.be>
2017-01-12 14:41:38 +0000

    Apply fixes from StyleCI

--- tests/RgbTest.php
+++ tests/RgbTest.php
@@ -2,8 +2,8 @@
 
 namespace Spatie\Color\Test;
 
-use Spatie\Color\Exceptions\InvalidColorValue;
 use Spatie\Color\Rgb;
+use Spatie\Color\Exceptions\InvalidColorValue;
 
 class RgbTest extends \PHPUnit_Framework_TestCase
 {
