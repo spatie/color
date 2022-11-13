40177e53677dbb4f324e44fb6b8231c99ab0786a
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-13 20:52:20 +0300

    refactor DistanceTest

--- tests/ContrastTest.php
+++ tests/ContrastTest.php
@@ -2,11 +2,12 @@
 
 use Spatie\Color\Color;
 use Spatie\Color\Contrast;
+use function PHPUnit\Framework\assertSame;
 
 it('can calculate contrast', function (Color $a, Color $b, float $contrast) {
-    $this->assertSame($contrast, Contrast::ratio($a, $b));
+    assertSame($contrast, Contrast::ratio($a, $b));
 })->with('contrast_colors');
 
 it('can calculate contrast from another format', function (Color $a, Color $b, float $contrast) {
-    $this->assertSame($contrast, Contrast::ratio($a->toRgba(), $b->toHsl()));
+    assertSame($contrast, Contrast::ratio($a->toRgba(), $b->toHsl()));
 })->with('contrast_colors');
--- tests/DistanceTest.php
+++ tests/DistanceTest.php
@@ -1,65 +1,48 @@
 <?php
 
-namespace Spatie\Color\Test;
-
-use PHPUnit\Framework\TestCase;
 use Spatie\Color\Distance;
 use Spatie\Color\Hex;
 use Spatie\Color\Rgb;
+use function PHPUnit\Framework\assertSame;
 
-class DistanceTest extends TestCase
-{
-    /** @test */
-    public function it_can_compare_distance_using_CIE76()
-    {
-        $color1 = Rgb::fromString('rgb(55,155,255)');
-        $color2 = Hex::fromString('#2d78c8');
-        $distance = Distance::CIE76($color1, $color2);
+it('can compare distance using CIE76', function () {
+    $color1 = Rgb::fromString('rgb(55,155,255)');
+    $color2 = Hex::fromString('#2d78c8');
+    $distance = Distance::CIE76($color1, $color2);
 
-        $this->assertSame(16.35058714542080, $distance);
-    }
+    assertSame(16.35058714542080, $distance);
+});
 
-    /** @test */
-    public function it_can_compare_distance_using_CIE76_and_string_colors()
-    {
-        $distance = Distance::CIE76('rgb(55,155,255)', '#2d78c8');
+it('can compare distance using CIE76 and string colors', function () {
+    $distance = Distance::CIE76('rgb(55,155,255)', '#2d78c8');
 
-        $this->assertSame(16.35058714542080, $distance);
-    }
+    assertSame(16.35058714542080, $distance);
+});
 
-    /** @test */
-    public function it_can_compare_distance_using_CIE94()
-    {
-        $color1 = Rgb::fromString('rgb(55,155,255)');
-        $color2 = Hex::fromString('#2d78c8');
-        $distance = Distance::CIE94($color1, $color2);
+it('can compare distance using CIE94', function () {
+    $color1 = Rgb::fromString('rgb(55,155,255)');
+    $color2 = Hex::fromString('#2d78c8');
+    $distance = Distance::CIE94($color1, $color2);
 
-        $this->assertSame(13.49091942790753, $distance);
-    }
+    assertSame(13.49091942790753, $distance);
+});
 
-    /** @test */
-    public function it_can_compare_distance_using_CIE94_and_string_colors()
-    {
-        $distance = Distance::CIE94('rgb(55,155,255)', '#2d78c8');
+it('can compare distance using CIE94 and string colors', function () {
+    $distance = Distance::CIE94('rgb(55,155,255)', '#2d78c8');
 
-        $this->assertSame(13.49091942790753, $distance);
-    }
+    assertSame(13.49091942790753, $distance);
+});
 
-    /** @test */
-    public function it_can_compare_distance_using_CIEDE2000()
-    {
-        $color1 = Rgb::fromString('rgb(55,155,255)');
-        $color2 = Hex::fromString('#2d78c8');
-        $distance = Distance::CIEDE2000($color1, $color2);
+it('can compare distance using CIEDE2000', function () {
+    $color1 = Rgb::fromString('rgb(55,155,255)');
+    $color2 = Hex::fromString('#2d78c8');
+    $distance = Distance::CIEDE2000($color1, $color2);
 
-        $this->assertSame(12.711957696300898, $distance);
-    }
+    assertSame(12.711957696300898, $distance);
+});
 
-    /** @test */
-    public function it_can_compare_distance_using_CIEDE2000_and_string_colors()
-    {
-        $distance = Distance::CIEDE2000('rgb(55,155,255)', '#2d78c8');
+it('can compare distance using CIEDE2000 and string colors', function () {
+    $distance = Distance::CIEDE2000('rgb(55,155,255)', '#2d78c8');
 
-        $this->assertSame(12.711957696300898, $distance);
-    }
-}
+    assertSame(12.711957696300898, $distance);
+});
