dccc792e1f77b0f3334bc19398786bb012e29feb
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-11-13 21:21:10 +0300

    refactor RgbaTest

--- tests/RgbaTest.php
+++ tests/RgbaTest.php
@@ -1,217 +1,174 @@
 <?php
 
-namespace Spatie\Color\Test;
-
-use PHPUnit\Framework\TestCase;
 use Spatie\Color\Exceptions\InvalidColorValue;
 use Spatie\Color\Rgba;
+use function PHPUnit\Framework\assertInstanceOf;
+use function PHPUnit\Framework\assertNotSame;
+use function PHPUnit\Framework\assertSame;
 
-class RgbaTest extends TestCase
-{
-    /** @test */
-    public function it_is_initializable()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
+it('is initializable', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
 
-        $this->assertInstanceOf(Rgba::class, $rgba);
-        $this->assertSame(55, $rgba->red());
-        $this->assertSame(155, $rgba->green());
-        $this->assertSame(255, $rgba->blue());
-        $this->assertSame(0.5, $rgba->alpha());
-    }
+    assertInstanceOf(Rgba::class, $rgba);
+    assertSame(55, $rgba->red());
+    assertSame(155, $rgba->green());
+    assertSame(255, $rgba->blue());
+    assertSame(0.5, $rgba->alpha());
+});
 
-    /** @test */
-    public function it_cant_be_initialized_with_a_negative_color_value()
-    {
-        $this->expectException(InvalidColorValue::class);
+it('cant be initialized with a negative color value', function () {
+    $this->expectException(InvalidColorValue::class);
 
-        new Rgba(-5, 255, 255, 0.5);
-    }
+    new Rgba(-5, 255, 255, 0.5);
+});
 
-    /** @test */
-    public function it_cant_be_initialized_with_a_color_value_higher_than_255()
-    {
-        $this->expectException(InvalidColorValue::class);
+it('cant be initialized with a color value higher than 255', function () {
+    $this->expectException(InvalidColorValue::class);
 
-        new Rgba(300, 255, 255, 0.5);
-    }
+    new Rgba(300, 255, 255, 0.5);
+});
 
-    /** @test */
-    public function it_cant_be_initialized_with_a_negative_alpha_value()
-    {
-        $this->expectException(InvalidColorValue::class);
+it('cant be initialized with a negative alpha value', function () {
+    $this->expectException(InvalidColorValue::class);
 
-        new Rgba(255, 255, 255, -1);
-    }
+    new Rgba(255, 255, 255, -1);
+});
 
-    /** @test */
-    public function it_cant_be_initialized_with_an_alpha_value_higher_than_1()
-    {
-        $this->expectException(InvalidColorValue::class);
+it('cant be initialized with an alpha value higher than 1', function () {
+    $this->expectException(InvalidColorValue::class);
 
-        new Rgba(255, 255, 255, 1.5);
-    }
+    new Rgba(255, 255, 255, 1.5);
+});
 
-    /** @test */
-    public function it_can_be_created_from_a_string()
-    {
-        $rgba = Rgba::fromString('rgba(55,155,255,0.5)');
+it('can be created from a string', function () {
+    $rgba = Rgba::fromString('rgba(55,155,255,0.5)');
 
-        $this->assertInstanceOf(Rgba::class, $rgba);
-        $this->assertSame(55, $rgba->red());
-        $this->assertSame(155, $rgba->green());
-        $this->assertSame(255, $rgba->blue());
-        $this->assertSame(0.5, $rgba->alpha());
-    }
+    assertInstanceOf(Rgba::class, $rgba);
+    assertSame(55, $rgba->red());
+    assertSame(155, $rgba->green());
+    assertSame(255, $rgba->blue());
+    assertSame(0.5, $rgba->alpha());
+});
 
-    /** @test */
-    public function it_can_be_created_with_an_opacity_value_without_leading_zero()
-    {
-        $rgba = Rgba::fromString('rgba(55,155,255,.555)');
+it('can be created with an opacity value without leading zero', function () {
+    $rgba = Rgba::fromString('rgba(55,155,255,.555)');
 
-        $this->assertInstanceOf(Rgba::class, $rgba);
-        $this->assertSame(55, $rgba->red());
-        $this->assertSame(155, $rgba->green());
-        $this->assertSame(255, $rgba->blue());
-        $this->assertSame(.555, $rgba->alpha());
-    }
+    assertInstanceOf(Rgba::class, $rgba);
+    assertSame(55, $rgba->red());
+    assertSame(155, $rgba->green());
+    assertSame(255, $rgba->blue());
+    assertSame(.555, $rgba->alpha());
+});
 
-    /** @test */
-    public function it_can_be_created_from_a_string_with_3_decimals_in_opacity()
-    {
-        $rgba = Rgba::fromString('rgba(55,155,255,0.555)');
+it('can be created from a string with 3 decimals in opacity', function () {
+    $rgba = Rgba::fromString('rgba(55,155,255,0.555)');
 
-        $this->assertInstanceOf(Rgba::class, $rgba);
-        $this->assertSame(55, $rgba->red());
-        $this->assertSame(155, $rgba->green());
-        $this->assertSame(255, $rgba->blue());
-        $this->assertSame(0.555, $rgba->alpha());
-    }
+    assertInstanceOf(Rgba::class, $rgba);
+    assertSame(55, $rgba->red());
+    assertSame(155, $rgba->green());
+    assertSame(255, $rgba->blue());
+    assertSame(0.555, $rgba->alpha());
+});
 
-    /** @test */
-    public function it_can_be_created_from_a_string_with_spaces()
-    {
-        $rgba = Rgba::fromString('  rgba(  55  ,  155  ,  255  ,  0.5  )  ');
+it('can be created from a string with spaces', function () {
+    $rgba = Rgba::fromString('  rgba(  55  ,  155  ,  255  ,  0.5  )  ');
 
-        $this->assertInstanceOf(Rgba::class, $rgba);
-        $this->assertSame(55, $rgba->red());
-        $this->assertSame(155, $rgba->green());
-        $this->assertSame(255, $rgba->blue());
-        $this->assertSame(0.5, $rgba->alpha());
-    }
+    assertInstanceOf(Rgba::class, $rgba);
+    assertSame(55, $rgba->red());
+    assertSame(155, $rgba->green());
+    assertSame(255, $rgba->blue());
+    assertSame(0.5, $rgba->alpha());
+});
 
-    /** @test */
-    public function it_cant_be_created_from_malformed_string()
-    {
-        $this->expectException(InvalidColorValue::class);
+it('cant be created from malformed string', function () {
+    $this->expectException(InvalidColorValue::class);
 
-        Rgba::fromString('rgba(55,155,255,0.5');
-    }
+    Rgba::fromString('rgba(55,155,255,0.5');
+});
 
-    /** @test */
-    public function it_cant_be_created_from_a_string_with_text_around()
-    {
-        $this->expectException(InvalidColorValue::class);
+it('cant be created from a string with text around', function () {
+    $this->expectException(InvalidColorValue::class);
 
-        Rgba::fromString('abc rgba(55,155,255,0.5) abc');
-    }
+    Rgba::fromString('abc rgba(55,155,255,0.5) abc');
+});
 
-    /** @test */
-    public function it_can_be_casted_to_a_string()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
+it('can be casted to a string', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
 
-        $this->assertSame('rgba(55,155,255,0.50)', (string) $rgba);
-    }
+    assertSame('rgba(55,155,255,0.50)', (string) $rgba);
+});
 
-    /** @test */
-    public function it_can_be_converted_to_CIELab()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
-        $lab = $rgba->toCIELab();
+it('can be converted to CIELab', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
+    $lab = $rgba->toCIELab();
 
-        $this->assertSame(62.91, $lab->l());
-        $this->assertSame(5.34, $lab->a());
-        $this->assertSame(-57.73, $lab->b());
-    }
+    assertSame(62.91, $lab->l());
+    assertSame(5.34, $lab->a());
+    assertSame(-57.73, $lab->b());
+});
 
-    /** @test */
-    public function it_can_be_converted_to_cmyk()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
-        $cmyk = $rgba->toCmyk();
+it('can be converted to cmyk', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
+    $cmyk = $rgba->toCmyk();
 
-        $this->assertSame($rgba->red(), $cmyk->red());
-        $this->assertSame($rgba->green(), $cmyk->green());
-        $this->assertSame($rgba->blue(), $cmyk->blue());
-    }
+    assertSame($rgba->red(), $cmyk->red());
+    assertSame($rgba->green(), $cmyk->green());
+    assertSame($rgba->blue(), $cmyk->blue());
+});
 
-    /** @test */
-    public function it_can_be_converted_to_rgba_with_with_a_specific_alpha_value()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
-        $newRgba = $rgba->toRgba(0.7);
+it('can be converted to rgba with with a specific alpha value', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
+    $newRgba = $rgba->toRgba(0.7);
 
-        $this->assertSame(55, $newRgba->red());
-        $this->assertSame(155, $newRgba->green());
-        $this->assertSame(255, $newRgba->blue());
-        $this->assertSame(0.7, $newRgba->alpha());
-        $this->assertNotSame($rgba, $newRgba);
-    }
+    assertSame(55, $newRgba->red());
+    assertSame(155, $newRgba->green());
+    assertSame(255, $newRgba->blue());
+    assertSame(0.7, $newRgba->alpha());
+    assertNotSame($rgba, $newRgba);
+});
 
-    /** @test */
-    public function it_can_be_converted_to_rgb_without_an_alpha_value()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
-        $rgb = $rgba->toRgb();
+it('can be converted to rgb without an alpha value', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
+    $rgb = $rgba->toRgb();
 
-        $this->assertSame(55, $rgb->red());
-        $this->assertSame(155, $rgb->green());
-        $this->assertSame(255, $rgb->blue());
-    }
+    assertSame(55, $rgb->red());
+    assertSame(155, $rgb->green());
+    assertSame(255, $rgb->blue());
+});
 
-    /** @test */
-    public function it_can_be_converted_to_hex_without_an_alpha_value()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
-        $hex = $rgba->toHex();
+it('can be converted to hex without an alpha value', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
+    $hex = $rgba->toHex();
 
-        $this->assertSame('37', $hex->red());
-        $this->assertSame('9b', $hex->green());
-        $this->assertSame('ff', $hex->blue());
-    }
+    assertSame('37', $hex->red());
+    assertSame('9b', $hex->green());
+    assertSame('ff', $hex->blue());
+});
 
-    /** @test */
-    public function it_can_be_converted_to_hsl_without_an_alpha_value()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
-        $hsl = $rgba->toHsl();
+it('can be converted to hsl without an alpha value', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
+    $hsl = $rgba->toHsl();
 
-        $this->assertSame(55, $hsl->red());
-        $this->assertSame(155, $hsl->green());
-        $this->assertSame(255, $hsl->blue());
-    }
+    assertSame(55, $hsl->red());
+    assertSame(155, $hsl->green());
+    assertSame(255, $hsl->blue());
+});
 
-    /** @test */
-    public function it_can_be_converted_to_hsla_with_a_specific_alpha_value()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
-        $hsla = $rgba->toHsla(0.75);
+it('can be converted to hsla with a specific alpha value', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
+    $hsla = $rgba->toHsla(0.75);
 
-        $this->assertSame(55, $hsla->red());
-        $this->assertSame(155, $hsla->green());
-        $this->assertSame(255, $hsla->blue());
-        $this->assertSame(0.75, $hsla->alpha());
-    }
+    assertSame(55, $hsla->red());
+    assertSame(155, $hsla->green());
+    assertSame(255, $hsla->blue());
+    assertSame(0.75, $hsla->alpha());
+});
 
-    /** @test */
-    public function it_can_be_converted_to_xyz()
-    {
-        $rgba = new Rgba(55, 155, 255, 0.5);
-        $xyz = $rgba->toXyz();
+it('can be converted to xyz', function () {
+    $rgba = new Rgba(55, 155, 255, 0.5);
+    $xyz = $rgba->toXyz();
 
-        $this->assertSame(31.3469, $xyz->x());
-        $this->assertSame(31.4749, $xyz->y());
-        $this->assertSame(99.0308, $xyz->z());
-    }
-}
+    assertSame(31.3469, $xyz->x());
+    assertSame(31.4749, $xyz->y());
+    assertSame(99.0308, $xyz->z());
+});
