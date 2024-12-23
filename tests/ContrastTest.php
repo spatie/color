fe44719aa787d03d0875d82917e9ff3b9ba4e1cd
Daniele Barbaro <barbaro.daniele@gmail.com>
2024-12-23 11:47:49 +0100

    Contrast and Luminance tests

--- tests/ContrastTest.php
+++ tests/ContrastTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Spatie\Color\Hex;
 use function PHPUnit\Framework\assertSame;
 
 use Spatie\Color\Color;
@@ -12,3 +13,17 @@ it('can calculate contrast', function (Color $a, Color $b, float $contrast) {
 it('can calculate contrast from another format', function (Color $a, Color $b, float $contrast) {
     assertSame($contrast, Contrast::ratio($a->toRgba(), $b->toHsl()));
 })->with('contrast_colors');
+
+it('calculates the luminance correctly for a white color', function () {
+    $white = Hex::fromString('#FFFFFF');
+    $luminance = Contrast::calculateLuminance($white);
+
+    expect($luminance)->toBe(1.0);
+});
+
+it('calculates the luminance correctly for a black color', function () {
+    $black = Hex::fromString('#000000');
+    $luminance = Contrast::calculateLuminance($black);
+
+    expect($luminance)->toBe(0.0);
+});
