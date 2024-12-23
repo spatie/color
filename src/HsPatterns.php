0945d679e8f120d3eb28850de9aa2ff524ddf6e2
Daniele Barbaro <barbaro.daniele@gmail.com>
2024-12-23 16:01:45 +0100

    Extended hls* handling with floats

---
+++ src/HsPatterns.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Spatie\Color;
+
+class HsPatterns
+{
+    private const HUE = '\d{1,3}';
+    private const COMPONENT = '\d{1,3}(?:\.\d+)?%?';
+    private const ALPHA = '[0-1](?:\.\d{1,2})?';
+
+    private const VALIDATION_PATTERNS = [
+        'hsb' => '/^ *hs[vb]\( *-?' . self::HUE . ' *, *' . self::COMPONENT . ' *, *' . self::COMPONENT . ' *\) *$/i',
+        'hsl' => '/^ *hsl\( *-?' . self::HUE . ' *, *' . self::COMPONENT . ' *, *' . self::COMPONENT . ' *\) *$/i',
+        'hsla' => '/^ *hsla\( *' . self::HUE . ' *, *' . self::COMPONENT . ' *, *' . self::COMPONENT . ' *, *' . self::ALPHA . ' *\) *$/i',
+    ];
+
+    private const EXTRACTION_PATTERNS = [
+        'hsb' => '/hs[vb]\( *(-?' . self::HUE . ') *, *(' . self::COMPONENT . ') *, *(' . self::COMPONENT . ') *\)/i',
+        'hsl' => '/hsl\( *(-?' . self::HUE . ') *, *(' . self::COMPONENT . ') *, *(' . self::COMPONENT . ') *\)/i',
+        'hsla' => '/hsla\( *(' . self::HUE . ') *, *(' . self::COMPONENT . ') *, *(' . self::COMPONENT . ') *, *(' . self::ALPHA . ') *\)/i',
+    ];
+
+    public static function getValidationPattern(string $type): string
+    {
+        if (!isset(self::VALIDATION_PATTERNS[$type])) {
+            throw new \InvalidArgumentException('Invalid color type: ' . $type);
+        }
+
+        return self::VALIDATION_PATTERNS[$type];
+    }
+
+    public static function getExtractionPattern(string $type): string
+    {
+        if (!isset(self::EXTRACTION_PATTERNS[$type])) {
+            throw new \InvalidArgumentException('Invalid color type: ' . $type);
+        }
+
+        return self::EXTRACTION_PATTERNS[$type];
+    }
+}
